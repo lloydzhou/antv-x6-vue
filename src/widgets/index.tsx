4cbc6c5d6b3ab1caea65e0062f86ea60c434607c
lloydzhou <lloydzhou@qq.com>
2022-04-25 13:36:39 +0800

    增加右键菜单

--- src/widgets/index.tsx
+++ src/widgets/index.tsx
@@ -2,11 +2,13 @@ import Snapline from './Snapline'
 import Selection from './Selection'
 import MiniMap from './MiniMap'
 import Stencil, { StencilGroup } from './Stencil'
+import ContextMenu, { useContextMenu } from './ContextMenu'
 
 export default {
   Snapline,
   MiniMap,
   Selection,
   Stencil, StencilGroup,
+  ContextMenu, useContextMenu,
 }
 
